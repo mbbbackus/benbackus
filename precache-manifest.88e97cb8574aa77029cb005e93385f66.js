11b7d207e50c39fe8f60308de76e6b95e70b37af
Michael Benjamin Backus <mbbbackus@gmail.com>
2020-07-29 16:17:10 -0400

    Updates

---
+++ precache-manifest.88e97cb8574aa77029cb005e93385f66.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810e067e7c7f97e8ed90f16e87abff89",
+    "url": "/benback.us/index.html"
+  },
+  {
+    "revision": "ba552b308ee54bc7eb1e",
+    "url": "/benback.us/static/css/main.3baaaf4e.chunk.css"
+  },
+  {
+    "revision": "1ec8b18f3998742dbf9e",
+    "url": "/benback.us/static/js/2.b58c1ff1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/benback.us/static/js/2.b58c1ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba552b308ee54bc7eb1e",
+    "url": "/benback.us/static/js/main.86f6a5eb.chunk.js"
+  },
+  {
+    "revision": "fbe32172fdb726d3a97b",
+    "url": "/benback.us/static/js/runtime-main.bd9f2be5.js"
+  },
+  {
+    "revision": "644cd32f8412a6ea6095bbebd65d663d",
+    "url": "/benback.us/static/media/Backus_Cognition.644cd32f.pdf"
+  },
+  {
+    "revision": "5c5745ffe312dd91ed135257d24990cf",
+    "url": "/benback.us/static/media/Backus_Love_Story.5c5745ff.pdf"
+  },
+  {
+    "revision": "be36facb25937601933d0e7574935aa9",
+    "url": "/benback.us/static/media/Backus_PenalLotteries.be36facb.pdf"
+  },
+  {
+    "revision": "0711b807305e476c8acb464a866bf8ac",
+    "url": "/benback.us/static/media/Backus_political_philosophy.0711b807.pdf"
+  },
+  {
+    "revision": "25a487641a850a8df1183d1b0ea47aa3",
+    "url": "/benback.us/static/media/Meta-realism.25a48764.pdf"
+  },
+  {
+    "revision": "073cb93f5be89c1c9938a525c1f4ebf7",
+    "url": "/benback.us/static/media/Metaphysics_complex_present_paper.073cb93f.pdf"
+  },
+  {
+    "revision": "d85d84f8d91c98cacfcbf9ea816d4bf7",
+    "url": "/benback.us/static/media/PTB_Outline.d85d84f8.pdf"
+  },
+  {
+    "revision": "73e83c00e6a48bcefe86b1973c72f4be",
+    "url": "/benback.us/static/media/PoFExcerpt.73e83c00.pdf"
+  },
+  {
+    "revision": "95771cf1c07f23ba676e7690a77167b4",
+    "url": "/benback.us/static/media/backus_resume.95771cf1.jpg"
+  },
+  {
+    "revision": "69bab9828052c13e910a8f175d5fdf7b",
+    "url": "/benback.us/static/media/benesia.69bab982.jpg"
+  },
+  {
+    "revision": "8da2288f0b4ae14df0d264993451529c",
+    "url": "/benback.us/static/media/charlie_and_the_toxic_factory.8da2288f.jpg"
+  },
+  {
+    "revision": "7514abd832895e510d910c7747f75b84",
+    "url": "/benback.us/static/media/claudio_y_jasna.7514abd8.jpg"
+  },
+  {
+    "revision": "58ae585ad17e151c9bc32d1f6029a111",
+    "url": "/benback.us/static/media/delight.58ae585a.jpg"
+  },
+  {
+    "revision": "53a5ee1c4a209a85d013c1a3c85770ec",
+    "url": "/benback.us/static/media/episode.53a5ee1c.jpg"
+  },
+  {
+    "revision": "14e53a3097aa54b769ed22112b905d4a",
+    "url": "/benback.us/static/media/frozen_sugar_storm.14e53a30.jpg"
+  },
+  {
+    "revision": "befa434f8c264c8380828288d23a0008",
+    "url": "/benback.us/static/media/in_gods_iris.befa434f.jpg"
+  },
+  {
+    "revision": "8bde647b98018ad57d6d11e10dd74319",
+    "url": "/benback.us/static/media/into_the_vanity.8bde647b.jpg"
+  },
+  {
+    "revision": "47dff05924642e6510c6a2dba31f406c",
+    "url": "/benback.us/static/media/jared.47dff059.jpg"
+  },
+  {
+    "revision": "9cbb40ce3084d0e48dd132bc1aa33e47",
+    "url": "/benback.us/static/media/magma_ocean_golden_hour.9cbb40ce.jpg"
+  },
+  {
+    "revision": "929a472326ec030f56f7ff957706dc9a",
+    "url": "/benback.us/static/media/mushroom_sky.929a4723.jpg"
+  },
+  {
+    "revision": "8b96b11d7e5377b6c3fcea55115b38ad",
+    "url": "/benback.us/static/media/pairwise_emotional_disregulation.8b96b11d.jpg"
+  },
+  {
+    "revision": "a9dc2b97b56ada826c12c6efdb6bd389",
+    "url": "/benback.us/static/media/phoenix_eye.a9dc2b97.jpg"
+  },
+  {
+    "revision": "c98247fade570e6afe9a4aa4574be056",
+    "url": "/benback.us/static/media/pop_flag.c98247fa.jpg"
+  },
+  {
+    "revision": "726c8c853c5ae64dea621281a73a328d",
+    "url": "/benback.us/static/media/profile_pic.726c8c85.jpg"
+  },
+  {
+    "revision": "e9829e09b1fa52ebcfbbdbb5ed23c878",
+    "url": "/benback.us/static/media/self_portrait.e9829e09.jpg"
+  },
+  {
+    "revision": "cfb89dfe8748ad4bd2d6830ec5743a4f",
+    "url": "/benback.us/static/media/textures_and_colors.cfb89dfe.jpg"
+  },
+  {
+    "revision": "c983952d6ba33b6d9bb25c5d28f5ad05",
+    "url": "/benback.us/static/media/through_all_the_walls.c983952d.jpg"
+  },
+  {
+    "revision": "7741db7fe92e2fe857f536dc7129dd4b",
+    "url": "/benback.us/static/media/time.7741db7f.jpg"
+  },
+  {
+    "revision": "d48943ce17892f7b35f3f9fad1dd0346",
+    "url": "/benback.us/static/media/to_the_sky.d48943ce.jpg"
+  },
+  {
+    "revision": "e423128afdb2e76f7a91aa22d7f24a68",
+    "url": "/benback.us/static/media/value_satur_hue.e423128a.jpg"
+  }
+]);
