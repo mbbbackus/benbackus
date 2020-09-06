b743036cca7cd56e24822c3f3c6ffa2aa6fa9a04
Michael Benjamin Backus <mbbbackus@gmail.com>
2020-09-06 17:00:04 -0400

    Updates

---
+++ precache-manifest.419bac43954a685d8f0a76796f5b6434.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993726c93fab547a894c60bf8baed1db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a931df1256d5d84585d",
+    "url": "/static/css/main.2227d51a.chunk.css"
+  },
+  {
+    "revision": "0ef9c67b9bdb42f00c67",
+    "url": "/static/js/2.4e90af4d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4e90af4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a931df1256d5d84585d",
+    "url": "/static/js/main.c5f49f6a.chunk.js"
+  },
+  {
+    "revision": "79c19766b9fafc23cac3",
+    "url": "/static/js/runtime-main.e387f811.js"
+  },
+  {
+    "revision": "785ba28c009a4c68a23e978e7083f931",
+    "url": "/static/media/AdHunterPoster.785ba28c.png"
+  },
+  {
+    "revision": "644cd32f8412a6ea6095bbebd65d663d",
+    "url": "/static/media/Backus_Cognition.644cd32f.pdf"
+  },
+  {
+    "revision": "5c5745ffe312dd91ed135257d24990cf",
+    "url": "/static/media/Backus_Love_Story.5c5745ff.pdf"
+  },
+  {
+    "revision": "be36facb25937601933d0e7574935aa9",
+    "url": "/static/media/Backus_PenalLotteries.be36facb.pdf"
+  },
+  {
+    "revision": "0cc8d968e21640291168f8a5c34af4e5",
+    "url": "/static/media/Backus_Resume.0cc8d968.pdf"
+  },
+  {
+    "revision": "0711b807305e476c8acb464a866bf8ac",
+    "url": "/static/media/Backus_political_philosophy.0711b807.pdf"
+  },
+  {
+    "revision": "25a487641a850a8df1183d1b0ea47aa3",
+    "url": "/static/media/Meta-realism.25a48764.pdf"
+  },
+  {
+    "revision": "073cb93f5be89c1c9938a525c1f4ebf7",
+    "url": "/static/media/Metaphysics_complex_present_paper.073cb93f.pdf"
+  },
+  {
+    "revision": "d85d84f8d91c98cacfcbf9ea816d4bf7",
+    "url": "/static/media/PTB_Outline.d85d84f8.pdf"
+  },
+  {
+    "revision": "73e83c00e6a48bcefe86b1973c72f4be",
+    "url": "/static/media/PoFExcerpt.73e83c00.pdf"
+  },
+  {
+    "revision": "1ad9b5ec7b55c65e9100ca2de71f64a7",
+    "url": "/static/media/WasteSensorSchematic.1ad9b5ec.png"
+  },
+  {
+    "revision": "69bab9828052c13e910a8f175d5fdf7b",
+    "url": "/static/media/benesia.69bab982.jpg"
+  },
+  {
+    "revision": "8da2288f0b4ae14df0d264993451529c",
+    "url": "/static/media/charlie_and_the_toxic_factory.8da2288f.jpg"
+  },
+  {
+    "revision": "7514abd832895e510d910c7747f75b84",
+    "url": "/static/media/claudio_y_jasna.7514abd8.jpg"
+  },
+  {
+    "revision": "58ae585ad17e151c9bc32d1f6029a111",
+    "url": "/static/media/delight.58ae585a.jpg"
+  },
+  {
+    "revision": "53a5ee1c4a209a85d013c1a3c85770ec",
+    "url": "/static/media/episode.53a5ee1c.jpg"
+  },
+  {
+    "revision": "14e53a3097aa54b769ed22112b905d4a",
+    "url": "/static/media/frozen_sugar_storm.14e53a30.jpg"
+  },
+  {
+    "revision": "befa434f8c264c8380828288d23a0008",
+    "url": "/static/media/in_gods_iris.befa434f.jpg"
+  },
+  {
+    "revision": "8bde647b98018ad57d6d11e10dd74319",
+    "url": "/static/media/into_the_vanity.8bde647b.jpg"
+  },
+  {
+    "revision": "47dff05924642e6510c6a2dba31f406c",
+    "url": "/static/media/jared.47dff059.jpg"
+  },
+  {
+    "revision": "9cbb40ce3084d0e48dd132bc1aa33e47",
+    "url": "/static/media/magma_ocean_golden_hour.9cbb40ce.jpg"
+  },
+  {
+    "revision": "929a472326ec030f56f7ff957706dc9a",
+    "url": "/static/media/mushroom_sky.929a4723.jpg"
+  },
+  {
+    "revision": "8b96b11d7e5377b6c3fcea55115b38ad",
+    "url": "/static/media/pairwise_emotional_disregulation.8b96b11d.jpg"
+  },
+  {
+    "revision": "a9dc2b97b56ada826c12c6efdb6bd389",
+    "url": "/static/media/phoenix_eye.a9dc2b97.jpg"
+  },
+  {
+    "revision": "c98247fade570e6afe9a4aa4574be056",
+    "url": "/static/media/pop_flag.c98247fa.jpg"
+  },
+  {
+    "revision": "726c8c853c5ae64dea621281a73a328d",
+    "url": "/static/media/profile_pic.726c8c85.jpg"
+  },
+  {
+    "revision": "59484a0b5e5064558fdfcaae2462f84f",
+    "url": "/static/media/robotImg.59484a0b.JPG"
+  },
+  {
+    "revision": "ba39763768ddbe3ed3f58411d6f5e29a",
+    "url": "/static/media/robotImg2.ba397637.JPG"
+  },
+  {
+    "revision": "e9829e09b1fa52ebcfbbdbb5ed23c878",
+    "url": "/static/media/self_portrait.e9829e09.jpg"
+  },
+  {
+    "revision": "cfb89dfe8748ad4bd2d6830ec5743a4f",
+    "url": "/static/media/textures_and_colors.cfb89dfe.jpg"
+  },
+  {
+    "revision": "c983952d6ba33b6d9bb25c5d28f5ad05",
+    "url": "/static/media/through_all_the_walls.c983952d.jpg"
+  },
+  {
+    "revision": "7741db7fe92e2fe857f536dc7129dd4b",
+    "url": "/static/media/time.7741db7f.jpg"
+  },
+  {
+    "revision": "d48943ce17892f7b35f3f9fad1dd0346",
+    "url": "/static/media/to_the_sky.d48943ce.jpg"
+  },
+  {
+    "revision": "e423128afdb2e76f7a91aa22d7f24a68",
+    "url": "/static/media/value_satur_hue.e423128a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.310a883de8f79f7dab03fc7b7a963577.js"
+  "/precache-manifest.419bac43954a685d8f0a76796f5b6434.js"
 );
 
 self.addEventListener('message', (event) => {
