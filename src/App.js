6fe37aa375f130ce5262a9abbb81318b62d6cde6
Michael Benjamin Backus <mbbbackus@gmail.com>
2020-09-06 17:58:47 -0400

    fix navbar

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
 import React, { Component } from "react";
-import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import CodePage from './components/CodePage';
 import ArtPage from './components/ArtPage';
 import WritingPage from './components/WritingPage';
 import { tabNames } from "./variables";
 import './mysite.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      currentTab: "Home"
-    }
-    this.switchTab = this.switchTab.bind(this);
-    this.renderPage = this.renderPage.bind(this);
-  }
-  switchTab (event) {
-    let tabIndex = event.target.value;
-    if (tabIndex >= 0 && tabIndex <= 3) {
-      this.setState({currentTab: tabNames[tabIndex]});
-    }
-  }
-  renderPage () {
-    switch (this.state.currentTab) {
-      case "Code":
-        return <CodePage/>;
-      case "Art":
-        return <ArtPage/>;
-      case "Writing":
-        return <WritingPage/>;
-      default:
-        return <HomePage/>;
-    }
-  }
-  render () {
-    return (
-      <div className="App">
-        <NavBar currentTab={this.state.currentTab} switchTab={this.switchTab}/>
-        <div className="page-content">
-          {this.renderPage()}
-        </div>
-      </div>
-    );
-  } 
+function App() {
+  return (
+    <main className="App">
+      <Router>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/code" component={CodePage} />
+        <Route path="/art" component={ArtPage} />
+        <Route path="/writing" component={WritingPage} />
+      </Router>
+    </main>
+  );
 }
 
 export default App;
--- src/components/ArtPage.js
+++ src/components/ArtPage.js
@@ -49,23 +49,33 @@ class ArtPage extends Component {
 
   render () {
     return (
-      <div className="artPage">
-        <div className="art-text-container">
-          <p className="art-text">
-            I’d like to preface my visual art by saying that I love color. 
-            Philosophically, it’s what provoked me to consider <a href="https://plato.stanford.edu/entries/qualia/">qualia</a> when 
-            thinking about AI, which has made the aesthetic experience of 
-            color much more rich for me. <br/><br/>
-
-            Before I learned about <a href="https://plato.stanford.edu/entries/qualia/">qualia</a> , most of my art was figurative 
-            and drawn in black and white. Since then, I’ve come to relish 
-            in the mindless and methodical process of designing abstract 
-            compositions while experimenting with underutilized color 
-            palettes. <br/><br/>
-          </p>
+      <div>
+        <div className="navbar">
+          <a className="nav-link" href="/">Home</a>
+          <a className="nav-link" href="/Code">Code</a>
+          <a className="nav-link selected-tab" href="/Art">Art</a>
+          <a className="nav-link" href="/Writing">Writing</a>
         </div>
-        <div className="art-section-container">
-          {this.renderArt()}
+        <div className="page-content">
+          <div className="artPage">
+            <div className="art-text-container">
+              <p className="art-text">
+                I’d like to preface my visual art by saying that I love color. 
+                Philosophically, it’s what provoked me to consider <a href="https://plato.stanford.edu/entries/qualia/">qualia</a> when 
+                thinking about AI, which has made the aesthetic experience of 
+                color much more rich for me. <br/><br/>
+
+                Before I learned about <a href="https://plato.stanford.edu/entries/qualia/">qualia</a> , most of my art was figurative 
+                and drawn in black and white. Since then, I’ve come to relish 
+                in the mindless and methodical process of designing abstract 
+                compositions while experimenting with underutilized color 
+                palettes. <br/><br/>
+              </p>
+            </div>
+            <div className="art-section-container">
+              {this.renderArt()}
+            </div>
+          </div>
         </div>
       </div>
     );
